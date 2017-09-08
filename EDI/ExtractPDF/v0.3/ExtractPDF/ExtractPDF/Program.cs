519400d5df750b2cecf0ea2958e244d70ae57849
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-09-08 14:04:02 +1200

    save changes

--- EDI/ExtractPDF/v0.3/ExtractPDF/ExtractPDF/Program.cs
+++ EDI/ExtractPDF/v0.3/ExtractPDF/ExtractPDF/Program.cs
@@ -47,9 +47,9 @@ namespace ExtractPDF
         static void Main(string[] args)
         {
             /*
-                * Console.Write() - display extended ascii chars?
-                * https://stackoverflow.com/questions/3948089/console-write-display-extended-ascii-chars
-                */
+            * Console.Write() - display extended ascii chars?
+            * https://stackoverflow.com/questions/3948089/console-write-display-extended-ascii-chars
+            */
             Console.OutputEncoding = Encoding.UTF8;
 
             string app_location = GetAppPath();
@@ -79,9 +79,9 @@ namespace ExtractPDF
 
             /* 
              https://stackoverflow.com/questions/83152/reading-pdf-documents-in-net 
-             Change to following Code :
+             ITextExtractionStrategy Strategy = new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy();
              */
-            
+
 
             using (PdfReader reader = new PdfReader(pdf_file))
             {
@@ -97,7 +97,7 @@ namespace ExtractPDF
                     /* Why are GetTextFromPage from iTextSharp returning longer and longer strings?
                        https://stackoverflow.com/questions/35911062/why-are-gettextfrompage-from-itextsharp-returning-longer-and-longer-strings 
                        */
-                    //ITextExtractionStrategy Strategy = new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy();
+                    
                     ITextExtractionStrategy Strategy = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
 
                     // get the whole page content
@@ -105,8 +105,8 @@ namespace ExtractPDF
 
                     page_reader.ProcessPage(page_content);
 
-                    // Debug : do first 2 pages testing
-                    //if (page_count == 2) break;
+                    // Debug : do first 3 pages testing
+                    //if (page_count == 1) break;
                 }
             }
         }
@@ -123,45 +123,138 @@ namespace ExtractPDF
         }
 
         readonly string [] _ignore_list  = { "Import Health Standard" };
-        
 
+        int start_count = -1;
         public void ProcessPage(string page_content)
         {
             // Split the page content into individual line
             string[] lines_content = page_content.Split('\n');
 
             line_count = 0;
-            bool start_flag = true;
+            bool ignore_word_found = true;
 
+
+            string[] ignored_words = { "Import Health Standard", "2017", "Schedule", "Page", "KEY", "CITES" };
+            string[] classification_words = { "Marine Invertebrates", "Freshwater ornamental fish",
+                                                "Marine ornamental fish", "Marine invertebrates"};
+            string[] marine_words = { "Hard corals", "Soft Corals", "Anenomes", "Clams", "Invertebrates" };
+            string[] key_words = { "Valid scientific name" };
             // Process line by line
+            
             foreach (string line in lines_content)
             {
-                Console.Write($"[{(++line_count).ToString().PadLeft(3, '0')}]");
+                ++line_count;
+                //Console.Write($"[{(++line_count).ToString().PadLeft(3, '0')}]");
 
-                start_flag = CheckIgnoredList(line);
+                ignore_word_found = CheckForWord(line, ignored_words);
 
-                if (start_flag)
-                    Console.Write(line);
+                if ( ignore_word_found != true) 
+                {
+                    if (CheckForWord(line, classification_words) == true)
+                    {
+                        Console.Write($"[{(line_count).ToString().PadLeft(3, '0')}]");
+                        Console.Write("\t{Classification}");
+                        Console.Write(line+"\n");
+                        start_count = -1;
+                    }
+                    else if (CheckForWord(line, key_words) == true)
+                    {
+                        Console.Write($"[{(line_count).ToString().PadLeft(3, '0')}]");
+                        Console.Write("\t{Key}");
+                        Console.Write(line + "\n");
 
-                Console.Write("\n");
+                        start_count = 0;
+                    }
+                    else if (CheckForWord(line, marine_words) == true)
+                    {
+                        Console.Write($"[{(line_count).ToString().PadLeft(3, '0')}]");
+                        Console.Write("\t{Marine Key}");
+                        Console.Write(line + "\n");
+
+                        start_count = 0;
+                    }
+                    else if (start_count != -1)
+                    {
+                        if (line.Trim() != String.Empty)
+                        {
+                            start_count++;
+                            Console.Write($"[{(line_count).ToString().PadLeft(3, '0')}]");
+                            Console.Write($"==>[{(start_count).ToString().PadLeft(3, '0')}]");
+
+                            StringBuilder builder = new StringBuilder();
+                            // break the line into text with a space as delimitor
+                            string[] texts = line.Split(' ');
+
+                            
+                            if (texts.Length == 4)
+                            {
+                                Console.Write("{" + line.Trim() + "}");
+                            }
+                                
+                            else
+                            {
+                                Console.Write("{"+texts[0] + " " + texts[1]+"}");
+
+                                if(texts.Length > 2)
+                                {
+                                    int index = line.IndexOf(texts[2]);
+                                    Console.Write("{" +  line.Substring(index).Trim() + "}");
+                                }
+                                
+                            }
+                                
+                            Console.Write("\n");
+                        }
+                        
+                    }
+                        
+                    //Console.Write(line);
+                }
+                    
+
+                //Console.Write("\n");
             }
             page_count++;
             // Debug
             Console.WriteLine($"Page {page_count} been precessed.");
         }
 
-        bool CheckIgnoredList(string line)
+        /* 
+            v0.3
+         */
+        /// <summary>
+        /// check the text in the input line against an igonre list
+        /// return true if found, else turn false
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        //bool CheckIgnoredList(string line)
+        //{
+        //    string[] _ignored_list = { "Import Health Standard", "2017", "Schedule", "Page", "KEY", "CITES" };
+        //    bool flag = false;
+        //    foreach (string text in _ignored_list)
+        //    {
+        //        if (line.Contains(text))
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return flag;
+
+        //}
+
+        /* v0.3 => 08/09/2017 */ 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        bool CheckForWord(string line, string[] word_list)
         {
-            string[] _ignored_list = { "Import Health Standard", "2017", "Schedule", "Page", "KEY", "CITES" };
-            string[] special_words = { "Valid scientific name" }; // { "Marine", "Freshwater", "Valid scientific name" };
-            bool flag = true;
-            foreach (string text in _ignored_list)
-            {
+            bool flag = false;
+            foreach (string text in word_list)
                 if (line.Contains(text))
-                {
-                    return false;
-                }
-            }
+                    return true;
             return flag;
         }
     }
