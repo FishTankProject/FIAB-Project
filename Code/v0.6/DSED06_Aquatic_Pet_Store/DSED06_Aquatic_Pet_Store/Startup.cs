ca7b80ad0e2813f5d009a4327996924a9f59417a
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-09-13 10:38:10 +1200

    save new project This project create a web app on azure plaform.

---
+++ Code/v0.6/DSED06_Aquatic_Pet_Store/DSED06_Aquatic_Pet_Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DSED06_Aquatic_Pet_Store.Startup))]
+namespace DSED06_Aquatic_Pet_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
