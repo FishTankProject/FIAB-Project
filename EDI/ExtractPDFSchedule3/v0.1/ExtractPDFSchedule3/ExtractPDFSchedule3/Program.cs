1384da69797c94126476b026a4f597127f733e9a
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-09-14 08:31:54 +1200

    save new project

---
+++ EDI/ExtractPDFSchedule3/v0.1/ExtractPDFSchedule3/ExtractPDFSchedule3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExtractPDFHelperNameSpace;
+
+namespace ExtractPDFSchedule3
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            string app_location = AppPathHelper.GetAppPath();
+            //Console.WriteLine(app_location);
+
+            // current pdf file name
+            string pdf_file = "\\PDF\\Schedule 3.pdf";
+
+            ReadPDFHelper.ReadPDF(app_location + pdf_file);
+
+            Console.WriteLine("Program Terminated.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EDI/ExtractPDFSchedule3/v0.1/ExtractPDFSchedule3/ExtractPDFSchedule3/Schedule3Extractor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtractPDFHelperNameSpace;
+
+
+namespace ExtractPDFHelperNameSpace
+{
+    public class Schedule3Extractor : BasePDFExtractor, PDFExtractorInterface
+    {
+        public void ProcessPage(string page_content)
+        {
+            // Split the page content into individual line
+            string[] lines_content = page_content.Split('\n');
+
+            /* Add new word into ignored_words */
+            int size = ignored_words.Length;
+            Array.Resize(ref ignored_words, size+2);
+            ignored_words[size] = "Schedule 3";
+            ignored_words[size+1] = "Part 2";
+
+            int line_count = 0;
+            bool ignore_word_found;
+
+            foreach (string line in lines_content)
+            {
+                Console.Write($"[{(++line_count).ToString().PadLeft(3, '0')}]");
+                //Console.Write("\n");
+                ignore_word_found = CheckForWord(line, ignored_words);
+
+                if (ignore_word_found != true)
+                {
+                    string[] texts = line.Split(' ');
+                    Console.Write($"{line}");
+                    
+                }
+                Console.WriteLine("");
+            }
+            page_count++;
+            // Debug
+            Console.WriteLine($"Page {page_count} been precessed.");
+            
+        }
+    }
+}
